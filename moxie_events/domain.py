2413b6e2ee1688e199c61ed7158a82d140e37fdc
Martin Filliau <martin@filliau.com>
2013-05-31 17:24:39 +0100

    Added some documentation

--- moxie_events/domain.py
+++ moxie_events/domain.py
@@ -5,6 +5,9 @@ SOLR_DATE_FORMAT = "%Y-%m-%dT%H:%M:%SZ"
 
 class Event(object):
     def __init__(self, uid):
+        """Event
+        :param uid: unique identifier of the event
+        """
         self.uid = uid
 
     uid = None
@@ -22,6 +25,9 @@ class Event(object):
     source_url = None
 
     def to_solr_dict(self):
+        """Get event as a dict with Solr keys
+        :return: dict
+        """
         data = {
             'source_name': self.source_url,
             'source_url': self.source_url,
@@ -33,9 +39,14 @@ class Event(object):
         }
         if self.location:
             data['event_location'] = self.location
+        return data
 
     @staticmethod
     def from_solr_dict(d):
+        """Get an Event domain object from a Solr response
+        :param d: dict with Solr values
+        :return: Event domain object
+        """
         event = Event(d['event_uid'])
         event.source_url = d['source_url']
         event.name = d['event_title']
@@ -48,8 +59,16 @@ class Event(object):
 
     @staticmethod
     def _parse_date(date):
+        """Parse date from Solr format
+        :param date: str (date from Solr)
+        :return: datetime object
+        """
         return datetime.strptime(date, SOLR_DATE_FORMAT)
 
     @staticmethod
     def _date_to_solr(date):
+        """Get the date in Solr format
+        :param date: datetime object
+        :return: str (date in Solr format)
+        """
         return date.strftime(SOLR_DATE_FORMAT)
--- moxie_events/services.py
+++ moxie_events/services.py
@@ -8,6 +8,10 @@ from .domain import Event
 class EventsService(Service):
 
     def get_events_for_day(self, dt):
+        """Get events starting the given day (between 00:00 and 23:59)
+        :param dt: datetime of the given day (used for day, month, year only)
+        :return: list of Event domain objects (empty list if no results)
+        """
         start = dt.replace(hour=0, minute=0, second=0)
         end = dt.replace(hour=23, minute=59, second=59)
         q = {'q': 'event_start:[{start} TO {end}]'.format(start=Event._date_to_solr(start), end=Event._date_to_solr(end))}
@@ -18,6 +22,10 @@ class EventsService(Service):
         return [Event.from_solr_dict(e) for e in results.results]
 
     def get_event(self, uid):
+        """Get an event by its unique identifier
+        :param uid: identifier of the event
+        :return: Event domain object or None if not found
+        """
         docs = searcher.get_by_ids([uid])
         if docs.results:
             return Event.from_solr_dict(docs.results[0])
