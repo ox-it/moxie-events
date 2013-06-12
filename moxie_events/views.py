8981cc04c029057df16c1d0936121f7fd2fdb0df
Martin Filliau <martin@filliau.com>
2013-06-12 16:11:35 +0100

    Added expire headers and caching

--- moxie_events/views.py
+++ moxie_events/views.py
@@ -1,8 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from flask import request
 
 from moxie.core.views import ServiceView, accepts
+from moxie.core.cache import cache, args_cache_key
 from moxie.core.exceptions import abort
 from werkzeug.wrappers import BaseResponse
 from moxie.core.representations import HAL_JSON, JSON
@@ -13,46 +14,53 @@ from .representations import HALEventRepresentation, HALEventsRepresentation
 
 class EventsToday(ServiceView):
 
+    expires = datetime.utcnow().replace(hour=23, minute=59, second=59)
+
+    @cache.cached(timeout=600)
     def handle_request(self):
         service = EventsService.from_context()
-        self.start = request.args.get('start', 0)
-        self.count = request.args.get('count', 35)
-        results, self.size = service.get_events_for_day(datetime.today(), self.start, self.count)
-        return results
+        start = request.args.get('start', 0)
+        count = request.args.get('count', 35)
+        results, size = service.get_events_for_day(datetime.today(), start, count)
+        return {'results': results, 'start': start, 'count': count, 'size': size}
 
     @accepts(JSON, HAL_JSON)
     def as_json(self, response):
-        return HALEventsRepresentation(response, request.url_rule.endpoint, self.start, self.count, self.size).as_json()
+        return HALEventsRepresentation(response['results'], request.url_rule.endpoint, response['start'],
+                                       response['count'], response['size']).as_json()
 
 
 class EventsForDate(ServiceView):
 
+    @cache.cached(timeout=600)
     def handle_request(self, year, month, day):
         service = EventsService.from_context()
-        self.start = request.args.get('start', 0)
-        self.count = request.args.get('count', 35)
-        self.year = year
-        self.month = month
-        self.day = day
+        start = request.args.get('start', 0)
+        count = request.args.get('count', 35)
         try:
             date = datetime(int(year), int(month), int(day))
         except ValueError as ve:
             return abort(400, ve.message)
         else:
-            results, self.size = service.get_events_for_day(date, self.start, self.count)
-            return results
+            results, size = service.get_events_for_day(date, start, count)
+            return {'results': results, 'start': start, 'count': count, 'size': size,
+                    'year': year, 'month': month, 'day': day}
 
     @accepts(JSON, HAL_JSON)
     def as_json(self, response):
         if issubclass(type(response), BaseResponse):
             return response
         else:
-            return HALEventsRepresentation(response, request.url_rule.endpoint, self.start, self.count, self.size,
-                                           day=self.day, month=self.month, year=self.year).as_json()
+            return HALEventsRepresentation(response['results'], request.url_rule.endpoint, response['start'],
+                                           response['count'], response['size'], day=response['day'],
+                                           month=response['month'], year=response['year']).as_json()
 
 
 class EventView(ServiceView):
 
+    expires = timedelta(days=3)
+
+    @cache.cached(timeout=600)
     def handle_request(self, ident):
         service = EventsService.from_context()
         event = service.get_event(ident)
